6d473483d57357c800478b7053f8a19ec6448657
Adilson Masocatto <adilson.masocatto@gmail.com>
2025-03-05 23:35:12 -0300

    aula 01

---
+++ cypress/e2e/gui/login01.cy.js
@@ -0,0 +1,13 @@
+describe('Login', () => {
+  it('passes', () => {
+    cy.visit('/users/sign_in')
+    cy.get('input[id="user_login"]')
+      .type('root')
+    cy.get('input[id="user_password"]')
+      .type('emily123')
+    cy.get('input[value="Sign in"]')
+      .click()
+    cy.get('H2[data-qa-selector="welcome_title_content"]')
+      .contains('Welcome to GitLab') 
+  })
+})
